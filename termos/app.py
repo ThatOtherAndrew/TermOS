098bb02aed9d522d18e5f81d658e7ffb604ab0fc
ThatOtherAndrew <stroev.andrew@gmail.com>
2025-03-01 11:40:15 +0000

    Add header and footer

--- termos/app.py
+++ termos/app.py
@@ -1,6 +1,8 @@
 from textual.app import App, ComposeResult
+from textual.widgets import Header, Footer
 
 
 class TermOS(App):
     def compose(self) -> ComposeResult:
-        yield from ()
+        yield Header()
+        yield Footer()
