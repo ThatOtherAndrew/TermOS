fe38377874315cf6ab97ec8e6098c4511e1d8de6
ThatOtherAndrew <stroev.andrew@gmail.com>
2025-03-02 11:42:22 +0000

    Add language switcher to notepad app

--- termos/apps/notepad/main.py
+++ termos/apps/notepad/main.py
@@ -3,9 +3,9 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 from textual.app import ComposeResult
-from textual.containers import Center, HorizontalScroll
+from textual.containers import Center
 from textual.widget import Widget
-from textual.widgets import TextArea, Button, Label
+from textual.widgets import TextArea, Label, Select
 
 from termos.apps import OSApp
 
@@ -17,10 +17,15 @@ class NotepadWidget(Widget):
     def compose(self) -> ComposeResult:
         with Center():
             yield Label('<new file>')
-        yield TextArea(show_line_numbers=True)
-        with HorizontalScroll(classes='buttons'):
-            yield Button('Save', variant='primary')
-            yield Button('Save As...', variant='default')
+        editor = TextArea.code_editor()
+        yield editor
+        yield Select(
+            ((lang, lang) for lang in editor.available_languages),
+            prompt='Select language...',
+        )
+
+    def on_select_changed(self, message: Select.Changed) -> None:
+        self.query_one(TextArea).language = message.value
 
 
 class Notepad(OSApp):
